8b8b822e725926ba1919d1e71aa5fcced4dfedd1
Marc vd Made <marcvdmade01@gmail.com>
2021-03-24 13:04:50 +0100

    typo fix

--- screengreenmachine/src/App.js
+++ screengreenmachine/src/App.js
@@ -17,6 +17,7 @@ function App() {
         >
           Learn react
         </a>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </header>
     </div>
   );
