21190cb9f35527fa681bf3bc90e01cf0cd9117ab
Marc vd Made <marcvdmade01@gmail.com>
2021-03-24 12:20:12 +0100

    react setup

---
+++ screengreenmachine/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Screen Green Machine💚
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn react
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
