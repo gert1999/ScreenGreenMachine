14cff9b36ec56001f43e90026aaac56943e375ac
luuksg12 <0966746@hr.nl>
2021-04-21 11:16:51 +0200

    Added button lmao

--- src/components/MapPage.js
+++ src/components/MapPage.js
@@ -5,7 +5,10 @@ export class MapPage extends React.Component {
     render() {
         return (
             <div class="d-flex flex-row">
-                <div><Map/></div>
+                <div>
+                    <Map/>
+                <button class="testButton btn btn-success">TEST</button>
+                </div>
                 <div class="scoreBoard">
                     <h1>Screen Green Machine</h1>
                     <ul>
