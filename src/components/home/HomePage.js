a4d18cdde6ace962032fbc8b9bf5b19838a2658b
Marc vd Made <marcvdmade01@gmail.com>
2021-04-29 21:55:17 +0200

    fixed btn style

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -9,28 +9,26 @@ import { InfoDivs } from "./InfoDivs";
 export class HomePage extends React.Component {
     render() {
         return (
-                <div className="homeWrapper">
-                    <div className="d-flex flex-row justify-content-around flex-wrap conceptWrapper">
-                        <div>
-                            <h1>Screen Green Machine</h1>
-                            <div>
-                                <Link to="/map" className="mapBtn">Map</Link>
-                            </div>
-                        </div>
-                        <div className="d-flex flex-column uitlegDiv">
-                            <h2>Uitleg over het concept</h2>
-                            <p>
-                                Met deze map beoordelen we de
-                                hoeveelheid groen. Ook bepalen
-                                we doormiddel van een AI waar
-                                er het beste nog meer groen moet
-                                komen.
-                            </p>
-                        </div>
+            <div className="homeWrapper">
+                <div className="d-flex flex-row justify-content-around flex-wrap conceptWrapper">
+                    <div className="d-flex flex-column">
+                        <h1>Screen Green Machine</h1>
+                        <Link to="/map" className="mapBtn">Map</Link>
                     </div>
-                    <TeamHome />
-                    <InfoDivs />
+                    <div className="d-flex flex-column uitlegDiv">
+                        <h2>Uitleg over het concept</h2>
+                        <p>
+                            Met deze map beoordelen we de
+                            hoeveelheid groen. Ook bepalen
+                            we doormiddel van een AI waar
+                            er het beste nog meer groen moet
+                            komen.
+                            </p>
+                    </div>
                 </div>
+                <TeamHome />
+                <InfoDivs />
+            </div>
         )
     }
 }
