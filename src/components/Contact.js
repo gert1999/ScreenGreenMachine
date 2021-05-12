9f8d5c1368e28c7c0259181a3fbdeabf7a3d9e9a
DaphneBruggeman <0986809@hr.nl>
2021-05-12 12:13:57 +0200

    Linkedin added and taak

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,24 +20,24 @@ export class Contact extends React.Component {
                 <div className="team-container">
                     <div className="member-container"> 
                         <div className="member">
-                            <h2>Daphne Bruggeman</h2>
-                            <p>taak</p>
+                            <a href="https://www.linkedin.com/in/daphne-bruggeman-6306316b/">Daphne Bruggeman</a>
+                            <p>Onderzoekend technoloog</p>
                         </div>
                         <div className="member">
-                            <h2>Marc van de Made</h2>
-                            <p>taak</p>
+                            <a href="https://www.linkedin.com/in/marc-van-der-made-b15914193/">Marc van der Made</a>
+                            <p>Onderzoekend technoloog</p>
                         </div>
                         <div className="member">
-                            <h2>Quincy van der Sluis</h2>
-                            <p>taak</p>
+                            <a href="https://www.linkedin.com/in/quincy-van-der-sluis-863438156/">Quincy van der Sluis</a>
+                            <p>Betrokken technoloog</p>
                         </div>
                         <div className="member">
-                            <h2>Luuk 's Gravendijk</h2>
-                            <p>taak</p>
+                            <a href="https://www.linkedin.com/in/luuk-s-gravendijk-b9379219a/">Luuk 's Gravendijk</a>
+                            <p>Ontwerpend technoloog</p>
                         </div>
                         <div className="member">
-                            <h2>Gertjan Egas</h2>
-                            <p>taak</p>
+                            <a href="https://www.linkedin.com/in/gertjan-e-a875a912b/">Gertjan Egas</a>
+                            <p>Ondernemend technoloog</p>
                         </div>
                     </div>
 
